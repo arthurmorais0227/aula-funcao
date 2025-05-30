c3b80a686436eac7d85b667968313642f6fb19ae
Arthur Morais <mlkliso4000@gmail.com>
2025-05-30 16:25:19 -0300

    terceira e ultima leva

--- Exercícios/ex7.js
+++ Exercícios/ex7.js
@@ -1,5 +1,14 @@
 function filtrarPares(numero) {
-    if (numero % 2 == 0){
-        
+    let arrayCorreto = 0;
+    let arrayIncorreto = 0;
+
+    for(let i = 0; i < numero.length; i++) {
+        if (numero[i] % 2 == 0){
+            arrayCorreto.push(i);
     }
+
+    console.log(arrayCorreto);
 }
+}
+
+filtrarPares([0, 1, 2, 3, 4, 5, 6]);
